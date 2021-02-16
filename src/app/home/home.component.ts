a214e8761caff37c0e2bcf5ca77df8ca4cc84ea7
Guilherme Vitorino <guilherme.vitorino@indraenergia.com>
2021-02-16 19:25:46 -0300

    formatação dos estilos, adicionado conteudo a home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalComponent } from '../modal/modal.component'
+import { DomService } from 'src/app/services/dom.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
 
